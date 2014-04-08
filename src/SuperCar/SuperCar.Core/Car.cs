73c9fcb0d56f4ff4922f708d2c0105726569ab35
Dawlin Peña Luciano <dpena@credli.com>
2014-04-07 23:21:30 -0400

    Deleting the comment

--- src/SuperCar/SuperCar.Core/Car.cs
+++ src/SuperCar/SuperCar.Core/Car.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SuperCar.Core
+﻿namespace SuperCar.Core
 {
     public class Car
     {
         
-        //Linea Comentada
     }
 }
